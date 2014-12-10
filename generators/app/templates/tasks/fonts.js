0a1fc69ebfd804a1ff06aa260b1d4c1054805dc0
shimarulin <shimarulin@gmail.com>
2014-12-10 19:09:42 +0300

    Add fonts.convert.otf task

--- generators/app/templates/tasks/fonts.js
+++ generators/app/templates/tasks/fonts.js
@@ -19,3 +19,14 @@ gulp.task('fonts.files', [], function () {
 
 });
 
+gulp.task('fonts.convert.otf', [], function () {
+
+  return gulp.src(fonts.source.root.relative + "**/*.otf")
+    .pipe(flatten())
+    .pipe(otf2ttf())
+    .pipe(gulp.dest(function(file){
+      return fonts.source.root.relative + file.data.fontName
+    }));
+
+});
+
